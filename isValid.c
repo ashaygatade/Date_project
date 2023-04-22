35ec5a01eff826f8f856d08d183b91e619571d74
AshayGatade <ashaygatade@gmail.com>
2023-04-22 22:52:31 +0530

    Function to check if the given date is a valid date

--- isValid.c
+++ isValid.c
@@ -1,33 +1,57 @@
 					/*function to check if the date given from user is Valid or not*/
-#include<stdio.h>
+//#include<stdio.h>
 
-int isValid(int , int , int );
-/*Return value: SUCCESS = 0, FAILURE = -1*/
+/*int isValid(char *);*/
+/*Return value: SUCCESS = 1, FAILURE = 0*/
 
+#include"dtmanip.h"
 
-int main()
+int isValid(char *date)
 {
-	/*scanf("%d",);*/
-	isValid(int data, int month, int year);
-
-}
-
-int isValid(int dd, int mm, int yyyy)
-{
-	if(dd>=1 && dd>=31 && mm>=1 && mm<=12) {
+	splitDate(date, &dd, &mm, &yyyy);
+	if(dd>=1 && dd>=31 && mm>=1 && mm<=12 && yyyy>0) {
 		switch(mm) {
-		case 2:
-		case 4:
-		case 6:
-		case 9:
-		case 11:
+		case 1:	/*January*/
+			return 1;
+		case 2:	/*February*/
+			if( isLeap(yyyy) ) {	/*check for leap year*/
+			if( dd>=1 && dd <=29 )
+			return 1;
+			else return 0;
+
+			}
+
+			if( dd>=1 && dd <=28 )	/*if it not a leap year*/
+			return 1;
+			else return 0;
+
+		case 3:	/*March*/
+			return 1;
+		case 4:	/*April*/
 			if(dd==31)
-			return -1;
-		default:
 			return 0;
+		case 5:	/*May*/
+			return 1;
+		case 6:	/*June*/
+			if(dd==31)
+			return 0;
+		case 7:	/*July*/
+			return 1;
+		case 8:	/*August*/
+			return 1;
+		case 9:	/*September*/
+			if(dd==31)
+			return 0;
+		case 10: /*October*/
+			return 1;
+		case 11: /*November*/
+			if(dd==31)
+			return 0;
+		case 12: /*December*/
+			return 1;
 		}
 	}
 	else {
-		return 0;
+		return 0;			/*failure*/
 	}
 }
