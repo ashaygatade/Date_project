37bb804ba4ce6243e7c7bd7f9f907dd3cd7660db
AshayGatade <ashaygatade@gmail.com>
2023-04-22 22:56:37 +0530

    File containing Main function

---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>      /*for abs() function*/
+#include<string.h>
+#include"dtmanip.h"	/*includes the declaration of the useful functions*/
+
+int main()
+{
+	int choice;
+	while(1) {
+		printf("1: CHECK VALIDITY OF DATE\n2: \n3: COMPARING DATE\n4: EXTRACT DATE, MONTH AND YEAR FROM A DATE\n");
+		printf("5: CHECK FOR LEAP YEAR\n6: \n7: EXTRACT DATE, MONTH AND YEAR FROM A DATE\n");
+		printf("Enter your choice\n");
+		scanf("%d", &choice);
+
+		switch(choice) {
+		case 1:
+			;
+		case 2:
+			;
+		}
+	}
+}
